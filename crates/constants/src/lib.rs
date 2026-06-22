db7c7d92cb8a8fa617006c2c51b27b480b7fc041
levish0 <shiueo.csh@gmail.com>
2026-06-22 20:01:00 +0900

    fmt

--- crates/constants/src/lib.rs
+++ crates/constants/src/lib.rs
@@ -12,7 +12,8 @@ pub use cache_keys::{
     oauth_pending_key, oauth_pending_lock_key, oauth_state_key, password_reset_key,
     totp_used_code_key, EMAIL_CHANGE_PREFIX, EMAIL_SIGNUP_EMAIL_PREFIX, EMAIL_SIGNUP_HANDLE_PREFIX,
     EMAIL_VERIFICATION_PREFIX, OAUTH_PENDING_LOCK_PREFIX, OAUTH_PENDING_PREFIX, OAUTH_STATE_PREFIX,
-    OAUTH_STATE_TTL_SECONDS, PASSWORD_RESET_PREFIX, TOTP_USED_CODE_PREFIX, TOTP_USED_CODE_TTL_SECONDS,
+    OAUTH_STATE_TTL_SECONDS, PASSWORD_RESET_PREFIX, TOTP_USED_CODE_PREFIX,
+    TOTP_USED_CODE_TTL_SECONDS,
 };
 pub use moderation_actions::{
     moderation_action_to_string, string_to_moderation_action, ModerationAction,
